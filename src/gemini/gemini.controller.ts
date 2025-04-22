9d8a4bc6eef71f187912a2d11d79c79936a539fe
juanmarsilva <juanmartinsilva74@gmail.com>
2025-04-22 01:13:27 -0300

    md create new endpoint for basic-prompt-stream

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+
 import { GeminiService } from './gemini.service';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
+import { Response } from 'express';
 
 @Controller('gemini')
 export class GeminiController {
@@ -9,5 +11,25 @@ export class GeminiController {
   @Post('basic-prompt')
   async basicPrompt( @Body() basicPromptDto: BasicPromptDto ) {
     return this.geminiService.basicPrompt( basicPromptDto );
+  }
+  
+  @Post('basic-prompt-stream')
+  async basicPromptStream( 
+    @Body() basicPromptDto: BasicPromptDto,
+    @Res() res: Response,
+
+    // todo: files
+  ) {
+    const stream = await this.geminiService.basicPromptStream( basicPromptDto );
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(HttpStatus.OK);
+
+    for await ( const chunk of stream ) {
+      const piece = chunk.text;
+      res.write(piece);
+    };
+
+    res.end();
   }
 }
