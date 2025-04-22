c0658278ef7f6c5979fb65e52a7538f449b14774
juanmarsilva <juanmartinsilva74@gmail.com>
2025-04-21 23:47:08 -0300

    md refactor gemini service

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { GoogleGenAI } from "@google/genai";
 
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
+import { basicPromptUseCase } from './use-cases/basic-prompt.use-case';
 
 
 @Injectable()
@@ -11,16 +12,8 @@ export class GeminiService {
     private ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
     async basicPrompt( basicPromptDto: BasicPromptDto ) {
-        const response = await this.ai.models.generateContent({
-            model: "gemini-2.0-flash",
-            contents: basicPromptDto.prompt,
-            config: {
-                systemInstruction: 'Responde unicamente en español, en formato markdown',
-            },
-        });
-
-        return response.text;
-    }
+        return basicPromptUseCase( this.ai, basicPromptDto );
+    };
 
 
 }
