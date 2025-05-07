512a54f85945540e4ff568a2a613178483e766f7
juanmarsilva <juanmartinsilva74@gmail.com>
2025-05-07 00:33:03 -0300

    md implement serve static

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
-import { GeminiModule } from './gemini/gemini.module';
-import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { GeminiModule } from './gemini/gemini.module';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), GeminiModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+  
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    GeminiModule,
+  ],
   controllers: [],
   providers: [],
 })
